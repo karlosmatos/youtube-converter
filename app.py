3ce4b6e493c72cb171673cae8d9a27f9e81b33ed
Karel Malý <105454484+karlosmatos@users.noreply.github.com>
2023-11-17 20:00:18 +0100

    Update progress bar

--- app.py
+++ app.py
@@ -8,21 +8,18 @@ def download_audio_from_youtube(url):
     """Download audio from YouTube video and return the file path."""
     try:
         video = YouTube(url)
-        progress_text = "Downloading audio..."
-        progress_number = 0
-        progress_bar = st.progress(progress_number, text=progress_text)
+        progress_bar = st.progress(0, text="Downloading audio...")
 
         audio_stream = video.streams.get_audio_only()
-        progress_bar.progress(progress_number + 20, text=progress_text)
+        progress_bar.progress(20)
 
         title = video.title
-        photo_url = video.thumbnail_url
         st.write(f'Video title: {title}')  # Print the video title in Streamlit
-        st.image(photo_url, width=200)  # Print the video thumbnail in Streamlit
-        progress_bar.progress(progress_number + 30, text=progress_text)
+        st.image(video.thumbnail_url, width=200)  # Print the video thumbnail in Streamlit
+        progress_bar.progress(50)
 
         downloaded_file = audio_stream.download(filename=title)
-        progress_bar.progress(progress_number + 100, text=progress_text)
+        progress_bar.progress(100)
 
         renamed_file = f"{title}.mp3"
         os.rename(downloaded_file, renamed_file)

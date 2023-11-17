5873915751b329b6c7140012f5432f62d657637d
Karel Malý <105454484+karlosmatos@users.noreply.github.com>
2023-11-17 18:37:39 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,22 +2,32 @@ import streamlit as st
 from pytube import YouTube
 import os
 
-# Create a text input box
-url = st.text_input('Enter your youtube video url:')
+# Create a text input box for YouTube video URL
+youtube_url = st.text_input('Enter your YouTube video URL:')
 
-# Create a button
-button = st.button('Submit')
+# Create a submit button
+submit_button = st.button('Submit')
 
-# Action when button is pressed
-if button:
-    yt = YouTube(url)
-    video = yt.streams.get_audio_only()
-    video_title = yt.title
-    downloaded_file = video.download(filename=video_title)
-    new_file = f"{video_title}.mp3"
-    os.rename(downloaded_file, new_file)
-    with open(new_file, 'rb') as f:
-        data = f.read()
-    st.download_button(label='Download', data=data, file_name=new_file)
-    os.remove(new_file)
-    #test
+# Define action when submit button is pressed
+if submit_button:
+    # Initialize YouTube object with provided URL
+    youtube_video = YouTube(youtube_url)
+    
+    # Get audio stream of the video
+    audio_stream = youtube_video.streams.get_audio_only()
+    
+    # Get video title
+    video_title = youtube_video.title
+    
+    # Download the audio stream and rename it
+    downloaded_file = audio_stream.download(filename=video_title)
+    renamed_file = f"{video_title}.mp3"
+    os.rename(downloaded_file, renamed_file)
+    
+    # Read the downloaded file and provide a download button
+    with open(renamed_file, 'rb') as file:
+        file_data = file.read()
+    st.download_button(label='Download', data=file_data, file_name=renamed_file)
+    
+    # Remove the downloaded file after providing download button
+    os.remove(renamed_file)

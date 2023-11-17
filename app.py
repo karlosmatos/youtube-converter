91054bf9fc19fa9951d7d1c8db6bff03b02bcf7f
Karel Malý <105454484+karlosmatos@users.noreply.github.com>
2023-11-17 19:51:12 +0100

    Add progress bar, title, thumbnail

--- app.py
+++ app.py
@@ -5,13 +5,30 @@ from pytube.exceptions import VideoUnavailable
 
 
 def download_audio_from_youtube(url):
+    """Download audio from YouTube video and return the file path."""
     try:
         video = YouTube(url)
+        progress_text = "Downloading audio..."
+        progress_number = 0
+        progress_bar = st.progress(progress_number, text=progress_text)
+
         audio_stream = video.streams.get_audio_only()
+        progress_bar.progress(progress_number + 20, text=progress_text)
+
         title = video.title
+        photo_url = video.thumbnail_url
+        st.write(f'Video title: {title}')  # Print the video title in Streamlit
+        st.image(photo_url, width=200)  # Print the video thumbnail in Streamlit
+        progress_bar.progress(progress_number + 30, text=progress_text)
+
         downloaded_file = audio_stream.download(filename=title)
+        progress_bar.progress(progress_number + 100, text=progress_text)
+
         renamed_file = f"{title}.mp3"
         os.rename(downloaded_file, renamed_file)
+        progress_bar.empty()
+        st.success('Audio downloaded successfully!')
+
         return renamed_file
     
     except VideoUnavailable:

6857760234bda39e4a4675547ca1d64fa372e300
Karel Malý <105454484+karlosmatos@users.noreply.github.com>
2023-11-17 18:49:16 +0100

    Update code

--- app.py
+++ app.py
@@ -1,33 +1,42 @@
+import os
 import streamlit as st
 from pytube import YouTube
-import os
+from pytube.exceptions import VideoUnavailable
 
-# Create a text input box for YouTube video URL
-youtube_url = st.text_input('Enter your YouTube video URL:')
 
-# Create a submit button
-submit_button = st.button('Submit')
+def download_audio_from_youtube(url):
+    try:
+        video = YouTube(url)
+        audio_stream = video.streams.get_audio_only()
+        title = video.title
+        downloaded_file = audio_stream.download(filename=title)
+        renamed_file = f"{title}.mp3"
+        os.rename(downloaded_file, renamed_file)
+        return renamed_file
+    
+    except VideoUnavailable:
+        st.error('The video is not available. This could be due to region restrictions. Please try another video.')
+        return None
 
-# Define action when submit button is pressed
-if submit_button:
-    # Initialize YouTube object with provided URL
-    youtube_video = YouTube(youtube_url)
-    
-    # Get audio stream of the video
-    audio_stream = youtube_video.streams.get_audio_only()
-    
-    # Get video title
-    video_title = youtube_video.title
-    
-    # Download the audio stream and rename it
-    downloaded_file = audio_stream.download(filename=video_title)
-    renamed_file = f"{video_title}.mp3"
-    os.rename(downloaded_file, renamed_file)
-    
-    # Read the downloaded file and provide a download button
-    with open(renamed_file, 'rb') as file:
+
+def create_download_button_for_file(file_path):
+    with open(file_path, 'rb') as file:
         file_data = file.read()
-    st.download_button(label='Download', data=file_data, file_name=renamed_file)
-    
-    # Remove the downloaded file after providing download button
-    os.remove(renamed_file)
+
+    st.download_button(label='Download', data=file_data, file_name=file_path)
+    os.remove(file_path)
+
+
+def main():
+    youtube_url = st.text_input('Enter your YouTube video URL:')
+    submit_button = st.button('Submit')
+
+    if submit_button:
+        downloaded_file = download_audio_from_youtube(youtube_url)
+
+        if downloaded_file:
+            create_download_button_for_file(downloaded_file)
+
+
+if __name__ == "__main__":
+    main()

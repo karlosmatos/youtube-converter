f4a1d87e1879a340b2d26d93d5c0d9263dcf8c38
Karel Malý <105454484+karlosmatos@users.noreply.github.com>
2024-09-25 20:17:44 +0200

    Refactor code with yt-dlp + add Dockerfile

--- app.py
+++ app.py
@@ -2,21 +2,28 @@ import os
 import logging
 import requests
 import streamlit as st
-from pytube import YouTube
-from pytube.exceptions import VideoUnavailable
+import yt_dlp
 
 def download_audio_from_youtube(url):
     """Download audio from YouTube video and return the file path."""
+    ydl_opts = {
+        'format': 'bestaudio/best',
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '192',
+        }],
+        'outtmpl': '%(title)s.%(ext)s',
+    }
+    
     try:
-        video = YouTube(url)
-        audio_stream = video.streams.get_audio_only()
-        title = video.title
-        downloaded_file = audio_stream.download(filename=title)
-        renamed_file = f"{title}.mp3"
-        os.rename(downloaded_file, renamed_file)
-        return renamed_file
-    except VideoUnavailable:
-        st.error('The video is not available. This could be due to region restrictions or invalid URL. Please check URL or try another video.')
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            info = ydl.extract_info(url, download=False)
+            title = info['title']
+            ydl.download([url])
+            return f"{title}.mp3"
+    except Exception as e:
+        st.error(f'An error occurred: {str(e)}. Please check the URL or try another video.')
         return None
 
 def create_download_button_for_file(file_path):
@@ -32,23 +39,26 @@ def get_user_input():
     submit_button = st.button('Submit')
     return youtube_url, submit_button
 
-def log_user_activity(youtube_url):
-    """Log user activity."""
-    user_ip = requests.get('https://api.ipify.org').text
-    logging.info(f'User with IP {user_ip} submitted URL: {youtube_url}')
-
 def handle_video_download(youtube_url, progress_bar):
     """Handle video download and UI updates."""
-    video = YouTube(youtube_url)
-    progress_bar.progress(20)
-    st.write(f'Video title: {video.title}')
-    st.image(video.thumbnail_url, width=200)
-    progress_bar.progress(50)
-    downloaded_file = download_audio_from_youtube(youtube_url)
-    progress_bar.progress(100)
-    if downloaded_file:
-        create_download_button_for_file(downloaded_file)
-        st.success('Audio downloaded successfully!')
+    try:
+        with yt_dlp.YoutubeDL() as ydl:
+            info = ydl.extract_info(youtube_url, download=False)
+            title = info['title']
+            thumbnail_url = info['thumbnail']
+        
+        progress_bar.progress(20)
+        st.write(f'Video title: {title}')
+        st.image(thumbnail_url, width=200)
+        progress_bar.progress(50)
+        downloaded_file = download_audio_from_youtube(youtube_url)
+        progress_bar.progress(100)
+        if downloaded_file:
+            create_download_button_for_file(downloaded_file)
+            st.success('Audio downloaded successfully!')
+            progress_bar.empty()
+    except Exception as e:
+        st.error(f'An error occurred: {str(e)}. Please check the URL or try another video.')
         progress_bar.empty()
 
 def main():
@@ -58,7 +68,6 @@ def main():
     youtube_url, submit_button = get_user_input()
     if submit_button:
         progress_bar = st.progress(0, text="Downloading audio...")
-        log_user_activity(youtube_url)
         handle_video_download(youtube_url, progress_bar)
 
 if __name__ == "__main__":
